20be522af56d123d2f465cb2e59c16566ab42f65
Miguel Corona <migcorona23@gmail.com>
2025-12-17 11:52:44 -0600

    Add reading of sites into pandas dataframe

---
+++ src/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Distance(Enum):
+    NO_TRANSPORTATION = 0
+    WITHIN_IOWA_CITY_CORALVILLE_NORTH_LIBERTY = 8
+    UP_TO_15_MILES = 15
+    UP_TO_20_MILES = 20
+    UP_TO_30_MILES = 30
---
+++ src/main.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentParser, ArgumentTypeError
+import os
+import pandas as pd
+
+
+def csv_file(path: str) -> str:
+    if not os.path.exists(path):
+        raise ArgumentTypeError(f"File not found: {path}")
+
+    if not path.lower().endswith(".csv"):
+        raise ArgumentTypeError(f"Not a csv file: {path}")
+
+    return path
+
+
+def main(**kwargs):
+    if not (sites := kwargs.get("sites")):
+        print("No sites source provided.")
+        exit(1)
+
+    df_sites = pd.read_csv(sites)
+    print(df_sites.head())
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(description="Script to match students to sites")
+    arg_parser.add_argument(
+        "--sites", help="Csv storing the sites", type=csv_file, required=True
+    )
+
+    args = arg_parser.parse_args()
+
+    main(sites=args.sites)
